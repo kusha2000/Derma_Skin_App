87256371caae62a1ec2f716ff82bc7c4419070aa
AkelaDumindu <ikadthilakshana@std.appsc.sab.ac.lk>
2024-07-29 23:38:18 +0530

    created skin_type question pages

---
+++ lib/Pages/skin_type/skin_type_result.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:derma_skin_app/Pages/Risk_Profile/risk_answer_model.dart';
+
+class SkinTypeResult extends StatelessWidget {
+  final AnswerRiskModel answerModel;
+
+  SkinTypeResult({required this.answerModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Results')),
+      body: ListView.builder(
+        itemCount: answerModel.answers.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Question ${index + 1}'),
+            subtitle: Text('Answer: ${answerModel.answers[index]}'),
+          );
+        },
+      ),
+    );
+  }
+}
